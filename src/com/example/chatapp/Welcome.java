5c68a10360956bb15769874a34715e779bcb5aaa
prabhudamon <prabhu91raj@gmail.com>
2015-02-26 20:37:02 +0530

    Sign in And Login android application | SQLite DB
    A simple authentication android application with SQLite Database |
    Android Application

---
+++ src/com/example/chatapp/Welcome.java
@@ -0,0 +1,15 @@
+package com.example.chatapp;
+
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class Welcome extends Activity{
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.welcome);
+	}
+}
+
